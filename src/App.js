cef8ee372b89f48e1211dcdbfbe2e74ce901c160
rick habraken <rickhabraken@live.nl>
2025-01-08 10:43:11 +0100

    load pokemon data on page load

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './App.css';
-import pokemon from './pokemon.json'
 
 const PokemonRow = ({ pokemon, onSelect }) => (
   <tr>
@@ -58,7 +57,15 @@ PokemonInfo.propTypes = {
 
 function App() {
   const [filter, filterSet] = React.useState("");
+  const [pokemon, pokemonSet] = React.useState([]);
   const [selectedItem, selectedItemSet] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("http://localhost:3000/my-app/pokemon.json")
+    .then((resp) => resp.json())
+    .then((data) => pokemonSet(data))
+  }, [])
+
   return <div
     style={{
       margin: "auto",
@@ -95,7 +102,7 @@ function App() {
           </tbody>
         </table>
       </div>
-      {selectedItem && <PokemonInfo {... selectedItem} />}
+      {selectedItem && <PokemonInfo {...selectedItem} />}
     </div>
   </div>
 }
