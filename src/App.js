28625519cc68733b988000ab30e1602e684a13c6
rick habraken <rickhabraken@live.nl>
2025-01-08 11:20:34 +0100

    use mui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from "@emotion/styled"
+import Button from '@mui/material/Button';
+
 
 import './App.css';
 
@@ -9,7 +11,7 @@ const PokemonRow = ({ pokemon, onSelect }) => (
     <td>{pokemon.name.english}</td>
     <td>{pokemon.type.join(', ')}</td>
     <td>
-      <button onClick={() => onSelect(pokemon)}>Select!</button>
+      <Button onClick={() => onSelect(pokemon)}>Select!</Button>
     </td>
   </tr>
 );
@@ -25,7 +27,7 @@ PokemonRow.propTypes = {
 };
 
 const PokemonInfo = ({ name, base }) => (
-  <div>
+  <div>name
     <h1>{name.english}</h1>
     <table>
       {
@@ -64,7 +66,16 @@ const TwoColumnLayout = styled.div`
   grid-template-columns: 70% 30%;
   grid-column-gap: 1rem;
 `;
-
+const Container = styled.div`
+  margin: auto;
+  width: 800px;
+  paddingTop: 1rem;
+`;
+const Input = styled.input`
+  width: 100%;
+  font-size: x-large;
+  padding: 0.2rem;
+`
 
 function App() {
   const [filter, filterSet] = React.useState("");
@@ -77,40 +88,32 @@ function App() {
       .then((data) => pokemonSet(data))
   }, [])
 
-  return <div
-    style={{
-      margin: "auto",
-      width: 800,
-      paddingTop: "1rem",
-    }}
-  >
+  return <Container>
     <Title>Pokemon Search</Title>
-    <input
+    <Input
       value={filter}
       onChange={(evt) => filterSet(evt.target.value)}
     />
-    <div>
-      <TwoColumnLayout>
-        <table width="100%">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Type</th>
-            </tr>
-          </thead>
-          <tbody>
-            {pokemon
-              .filter((pokemon) => pokemon.name.english.toLocaleLowerCase().includes(filter))
-              .slice(0, 20)
-              .map((pokemon) => (
-                <PokemonRow pokemon={pokemon} key={pokemon.id} onSelect={(pokemon) => selectedItemSet(pokemon)} />
-              ))}
-          </tbody>
-        </table>
-      </TwoColumnLayout>
-      {selectedItem && <PokemonInfo {...selectedItem} />}
-    </div>
-  </div>
+    <TwoColumnLayout>
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pokemon
+            .filter((pokemon) => pokemon.name.english.toLocaleLowerCase().includes(filter))
+            .slice(0, 20)
+            .map((pokemon) => (
+              <PokemonRow pokemon={pokemon} key={pokemon.id} onSelect={(pokemon) => selectedItemSet(pokemon)} />
+            ))}
+        </tbody>
+      </table>
+    </TwoColumnLayout>
+    {selectedItem && <PokemonInfo {...selectedItem} />}
+  </Container>
 }
 
 export default App;
